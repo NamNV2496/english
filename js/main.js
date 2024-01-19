10d53d6348432171f7f50933119bcd91261aa1f4
Namnv2496 <namnv2496@gmail.com>
2024-01-19 23:14:13 +0700

    update questions

--- js/main.js
+++ js/main.js
@@ -75,7 +75,6 @@
     // Contact form validator
     $(function () {
 
-        $('#contact_form').validator();
 
         $('#contact_form').on('submit', function (e) {
             if (!e.isDefaultPrevented()) {
@@ -230,10 +229,6 @@
         var $portfolio_container = $(".portfolio-grid"),
             $gallery_container = $("#portfolio-gallery-grid");
 
-        $gallery_container.imagesLoaded(function () {
-            $gallery_container.masonry();
-        });
-
         $portfolio_container.imagesLoaded(function () {
             portfolio_init(this);
         });
@@ -241,13 +236,6 @@
         imageCarousel();
 
         // Blog grid init
-        var $container = $(".blog-masonry");
-        $container.imagesLoaded(function () {
-            $container.masonry({
-              itemSelector: '.item',
-              resize: false
-            });
-        });
 
         // Mobile menu
         $('.menu-toggle').on("click", function () {
@@ -255,134 +243,7 @@
             $('.site-nav').toggleClass('mobile-menu-hide');
         });
 
-        // Text rotation
-        $('.text-rotation').owlCarousel({
-            loop: true,
-            dots: false,
-            nav: false,
-            margin: 10,
-            items: 1,
-            autoplay: true,
-            autoplayHoverPause: false,
-            autoplayTimeout: 3800,
-            animateOut: 'fadeOut',
-            animateIn: 'fadeIn'
-        });
-
-        // Testimonials Slider
-        $(".testimonials.owl-carousel").owlCarousel({
-            nav: false, // Show next/prev buttons.
-            items: 3, // The number of items you want to see on the screen.
-            loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
-            navText: false,
-            margin: 25,
-            responsive : {
-                // breakpoint from 0 up
-                0 : {
-                    items: 1,
-                },
-                // breakpoint from 480 up
-                480 : {
-                    items: 1,
-                },
-                // breakpoint from 768 up
-                768 : {
-                    items: 2,
-                },
-                1200 : {
-                    items: 2,
-                }
-            }
-        });
-
-        // Clients Slider
-        $(".clients.owl-carousel").imagesLoaded().owlCarousel({
-            nav: false, // Show next/prev buttons.
-            items: 2, // The number of items you want to see on the screen.
-            loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
-            navText: false,
-            margin: 10,
-            autoHeight: false,
-            responsive : {
-                // breakpoint from 0 up
-                0 : {
-                    items: 2,
-                },
-                // breakpoint from 768 up
-                768 : {
-                    items: 4,
-                },
-                1200 : {
-                    items: 6,
-                }
-            }
-        });
-
-        // Lightbox init
-        body.magnificPopup({
-            fixedContentPos: false,
-            delegate: 'a.lightbox',
-            type: 'image',
-            removalDelay: 300,
-
-            // Class that is added to popup wrapper and background
-            // make it unique to apply your CSS animations just to this exact popup
-            mainClass: 'mfp-fade',
-            image: {
-                // options for image content type
-                titleSrc: 'title',
-                gallery: {
-                    enabled: true
-                },
-            },
-
-            iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                      '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-                patterns: {
-                    youtube: {
-                      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-                      id: null, // String that splits URL in a two parts, second part should be %id%
-                      // Or null - full URL will be returned
-                      // Or a function that should return %id%, for example:
-                      // id: function(url) { return 'parsed id'; }
-
-                      src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
-                    },
-                    vimeo: {
-                      index: 'vimeo.com/',
-                      id: '/',
-                      src: '//player.vimeo.com/video/%id%?autoplay=1'
-                    },
-                    gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
-                    }
-                },
-
-                srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-            },
-
-            callbacks: {
-                markupParse: function(template, values, item) {
-                 values.title = item.el.attr('title');
-                }
-            },
-        });
-
-        $('.ajax-page-load-link').magnificPopup({
-            type: 'ajax',
-            removalDelay: 300,
-            mainClass: 'mfp-fade',
-            gallery: {
-                enabled: true
-            },
-        });
+    
 
         $('.portfolio-page-wrapper .portfolio-grid').each(function() {
             $(this).magnificPopup({
@@ -422,14 +283,7 @@
             }, 400);
             return false;
         });
-        
-        //Google Maps
-        $("#map").googleMap({
-            zoom: 16 // Google Map ZOOM. You can change this value
-        });
-        $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
-        });
+    
 
         scrollTop();
 
